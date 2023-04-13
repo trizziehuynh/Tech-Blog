1b73f6f312a3e11e14e1ffddd99f659eb5306314
Trizzie Huynh <putmind2822@gmail.com>
2023-04-12 20:34:38 -0700

    added the models/index

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "comment_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
