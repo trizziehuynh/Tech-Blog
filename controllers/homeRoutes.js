4cf1968f168cf10c1a1981187f0df0714ee44a53
Trizzie Huynh <putmind2822@gmail.com>
2023-04-13 19:51:56 -0700

    homepage is able to load the posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  const postData = await Post.findAll({
+    include: [User, Comment],
+  });
+
+  let posts = postData.map((post) => post.get({ plain: true }));
+
+  res.render("homepage", {
+    posts,
+    logged_in: req.session.logged_in,
+    title: "homepage",
+    activie: { homepage: true },
+  });
 });
 
 module.exports = router;
